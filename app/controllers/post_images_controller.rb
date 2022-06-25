a4412d05b12cd0a5b596836320aa771f6e6bc61b
EC2 Default User <ec2-user@ip-172-31-22-182.ap-northeast-1.compute.internal>
2022-06-25 12:45:58 +0000

    [Fnish]4-18

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,7 +1,7 @@
 class PostImagesController < ApplicationController
 
  def new
-   @post_image = PostImage.new
+   @post_image=PostImage.new
 
  end  
  
@@ -18,6 +18,7 @@ class PostImagesController < ApplicationController
  
  def show
   @post_image=PostImage.find(params[:id])
+  @post_comment=PostComment.new
  end  
  
  def destroy
